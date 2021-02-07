cd4b3cc7910689188867e8efec74f96e4601b021
principalkelvoe <you@example.com>
2021-02-07 20:36:37 +0300

    add button and import addtodo.vue to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
@@ -8,13 +9,15 @@
 <script>
 import Header from './components/layout/Header';
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 
 export default {
   name: 'App',
   components: {
     Header,
-    Todos
+    Todos,
+    AddTodo
   },
   data(){
     return{
@@ -55,5 +58,18 @@ export default {
   body{
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+    }
+
+    .btn{
+      display: inline-block;
+      border: none;
+      background: #555;
+      color: #fff;
+      padding: 7px 20px;
+      cursor: pointer;
+    }
+
+    .btn:hover{
+      background: #666;
     }
 </style>
