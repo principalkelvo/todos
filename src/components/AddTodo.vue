df436ede774b12fc5cc3327763b8ef493f3f5859
principalkelvoe <you@example.com>
2021-02-07 20:30:01 +0300

    adding todo

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <form action="">
+            <input type="text" name="title" placeholder="Add Todo...">
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: "Addtodo"
+}
+</script>
+
+<style scoped>
+form{
+    display: flex;
+}
+input[type="text"]{
+    flex: 10;
+    padding: 5px;
+}
+input[type="submit"]{
+    flex: 2;
+}
+</style>
